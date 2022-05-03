4e049567531f6a84f55d6e61d454fa039f122a3c
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-05-02 17:40:41 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+//In this exercise, you’ll refactor some ES5 code into ES2015.
+
+//ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+//ES2015 Global Constants
+const PI = 3.14;
+PI = 42; //you can't change this with const.
+
+//Quiz
+//What is the difference between var and let?
+//var has function scope.
+
+//What is the difference between var and const?
+//var can reasign and redeclare a value, but const can't.
+
+//What is the difference between let and const?
+//let can reassifn and redeclare a value, but const can't.
+
+//What is hoisting?
+//Hoisting is the process where the declaration part of var is ran first at the top of the code before it is declared.
+
